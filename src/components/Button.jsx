c491c9c35bd609670fe08f63343f7d922c46a91e
akpevwe11 <essiakpevwepraise@gmail.com>
2023-01-16 10:48:17 +0100

    modern_banking_ui

---
+++ src/components/Button.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Button = ({styles}) =>
+    (
+        <div>
+            <button type='button' className={`py-4 px-6 bg-blue-gradient font-poppins font-medium text-[18px] text-primary outline-none rounded-lg ${styles}`}>Get Started</button>
+        </div>
+    )
+
+export default Button
